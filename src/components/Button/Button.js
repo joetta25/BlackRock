fa88c7172c0ada544fba0e379f6c5c3c0b483a5a
Dou Jones <MISTERCHIEFEXECUTIVEOFFICER@YAHOO.COM>
2019-09-24 10:57:08 -0400

    Add files via upload

---
+++ src/components/Button/Button.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+
+const white = "white";
+const padding = "padding: 1rem"
+const margin = "margin: 1rem";
+
+export const Button = styled.button`
+${padding}
+color: ${white}
+${margin}
+background: grey;
+text-transform: uppercase;
+left: 10%;
+bottom: 0;
+`
+
+export default Button
