9a360476564d94e18540c6041a19663df2dc7fee
Dou Jones <misterchiefexecutiveofficer@yahoo.com>
2019-09-24 15:07:08 -0400

    Front End
    Front End

---
+++ Front End/maps/build/precache-manifest.fca2daba4d5052ac392ca6e8c1ec60c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5f56ad588e5f2af722ff8181e808fdd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99717a8ae8c2b7e772d7",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b19f9594b20f26bb5809",
+    "url": "/static/css/main.288e88f9.chunk.css"
+  },
+  {
+    "revision": "99717a8ae8c2b7e772d7",
+    "url": "/static/js/2.ab2041a0.chunk.js"
+  },
+  {
+    "revision": "b19f9594b20f26bb5809",
+    "url": "/static/js/main.0d9eb167.chunk.js"
+  },
+  {
+    "revision": "25019f8bdd818ec40ae1",
+    "url": "/static/js/runtime~main.91c22b73.js"
+  }
+]);
